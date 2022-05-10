827053106ad0ca480b093a1b8812978f910ea4e4
yongjulejule <lyjshow200@gmail.com>
2022-05-10 15:56:28 +0900

    :sparkles: feat : [inception] add docker-compose practice

--- setup/install_docker.sh
+++ setup/install_docker.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-sudo apt-get reinstall gnupg
+echo -e  "\033[31mthis script should run as sudo! \033[0m"
+
+FLAG=`groups | grep -x "sudo"`
+
+sleep 1
+
+if [[ FLAG -eq 0 ]]
+then
+        echo "ok, you are in sudo group"
+else
+        echo "check permission :)"
+        exit 1
+fi
+
+# install docker
+sudo apt-get install gnupg
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get reinstall -y docker.io
+sudo apt-get install -y docker.io
 sudo chmod 666 /var/run/docker.sock
+ 
+# install docker-compose
+sudo aptitude install -y python3 python3-pip
+sudo aptitude install libffi-dev
+sudo pip3 install docker-compose
--- setup/set_vm.sh
+++ setup/set_vm.sh
@@ -3,6 +3,8 @@
 # begin as root
 echo -e  "\033[31mthis script should run as root! \033[0m"
 
+sleep 1
+
 user=`whoami`
 
 if [ $user = "root" ]
@@ -15,19 +17,18 @@ fi
 
 apt-get install -y update
 apt-get install -y aptitude
-aptitude install -y vim
 aptitude install -y sudo
 aptitude install -y apt-transport-https
 aptitude install -y ca-certificates
 aptitude install -y curl
 aptitude install -y software-propertirs-common
-aptitude install -y git
 aptitude install -y make
-aptitude install -y vim
 aptitude install -y systemd
 aptitude install -y ssh
 aptitude install -y ufw
 aptitude install -y apparmor
+aptitude install -y git
+aptitude install -y vim
 aptitude install -y net-tools
 aptitude install -y htop
 
