9dd57a7bed9ee50be5babfb0d9c99194bbd1cea4
yongjulejule <lyjshow200@gmail.com>
2022-05-10 12:30:30 +0900

    :hammer: chore : [inception] add file structure

--- setup/install_docker.sh
+++ setup/install_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install gnupg
+sudo apt-get reinstall gnupg
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt-get reinstall -y docker.io
 sudo chmod 666 /var/run/docker.sock
