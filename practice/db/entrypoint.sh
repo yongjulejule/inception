1619a36414962e3f5e2732b173ba9119400de373
yongjulejule <lyjshow200@gmail.com>
2022-05-19 21:07:54 +0900

    :memo: docs : [inception] modify Dockerfile but... dont work...😭😭😭

--- practice/db/entrypoint.sh
+++ practice/db/entrypoint.sh
@@ -2,27 +2,23 @@
 
 set -ex
 
-# if [ -d "/var/lib/mysql/mysql" ]; then
-# 	chown -R mysql:mysql /var/lib/mysql
-# else
+echo "Creating initial Mysql databases"
 
-	echo "Creating initial Mysql databases"
+MYSQL_DATABASE=${MYSQL_DATABASE:-"42-default-db"}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-"root"}
+MYSQL_USER=${MYSQL_USER:-"42-user"}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-"4242"}
 
-	MYSQL_DATABASE=${MYSQL_DATABASE:-"42-default-db"}
-	MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-"root"}
-	MYSQL_USER=${MYSQL_USER:-"42-user"}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-"4242"}
+# chown -R mysql:mysql /var/lib/mysql
+# mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-	
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		echo "MYSQL_ROOT_PASSWORD environment variable not exist" >&2
-	fi
+if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+	echo "MYSQL_ROOT_PASSWORD environment variable not exist" >&2
+fi
 
-	tmpfile=`mktemp`
+tmpfile=`mktemp`
 
-	cat << EOF > $tmpfile
+cat << EOF > $tmpfile
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
@@ -32,30 +28,29 @@ DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
 EOF
 
-	if [ "$MYSQL_DATABASE" != "" ]; then
+if [ "$MYSQL_DATABASE" != "" ]; then
 
-		MYSQL_CHARSET=${MYSQL_CHARSET:-utf8}
-		MYSQL_COLLATION=${MYSQL_COLLATION:-utf8_general_ci}
+	MYSQL_CHARSET=${MYSQL_CHARSET:-utf8}
+	MYSQL_COLLATION=${MYSQL_COLLATION:-utf8_general_ci}
 
-		echo "Creating database: $MYSQL_DATABASE"
-		echo "with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
-		echo "CREATE DATABASES IF NOT EXISTS '$MYSQL_DATABASE' CHARACTER SET ${MYSQL_CHARSET} COLLATE $MYSQL_COLLATION;" >> $tmpfile
+	echo "Creating database: $MYSQL_DATABASE"
+	echo "with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
+	echo "CREATE DATABASES IF NOT EXISTS '$MYSQL_DATABASE' CHARACTER SET ${MYSQL_CHARSET} COLLATE $MYSQL_COLLATION;" >> $tmpfile
 
-		if [ "$MYSQL_USER" != "" ]; then
-			echo "Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-			echo "GRANT ALL ON '$MYSQL_DATABASE'.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tmpfile
-		fi
+	if [ "$MYSQL_USER" != "" ]; then
+		echo "Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+		echo "GRANT ALL ON '$MYSQL_DATABASE'.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tmpfile
 	fi
+fi
 
-	echo
-	cat $tmpfile
-	echo
+cat $tmpfile
 
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tmpfile
-	rm -f $tmpfile
+/usr/bin/mysqld_safe --datadir=/var/lib/data&
 
-	echo "Mariadb init process done."
+mariadb < $tmpfile
 
-# fi
+rm -f $tmpfile
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+echo "Mariadb init process done."
+
+exec /usr/bin/mysqld_safe --datadir=/var/lib/mysql "$@"
